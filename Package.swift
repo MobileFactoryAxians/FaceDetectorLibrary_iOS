367b56fe82dcd22b6e8da06b1b66e9ea9802dd5f
Alexandre Carvalho <alexandre.oliveira-carvalho@axians.com>
2024-01-25 17:31:15 +0000

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "FaceDetectorLibrary",
+    platforms: [
+            .iOS(.v15),
+        ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "FaceDetectorLibrary",
+            targets: ["MediaPipeTasksVision"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "MediaPipeTasksVision",
+            path: "MediaPipeTasksVision.xcframework")
+    ]
+)
