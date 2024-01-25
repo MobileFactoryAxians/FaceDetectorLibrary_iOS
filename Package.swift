15dc72be0a7a7f77a15d145231d9cc48a6d22d76
Alexandre Carvalho <alexandre.oliveira-carvalho@axians.com>
2024-01-25 19:12:52 +0000

    New try

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "FaceDetectorLibrary_iOS",
-            targets: ["MediaPipeTasksVision"]),
+            targets: ["FaceDetectorLibrary_iOS", "MediaPipeTasksVision"]),
     ],
     targets: [
         .target(name: "FaceDetectorLibrary_iOS",
